496d86fd0f7772c7996d8b3e32edd3741c1c41ef
Finlay Collins <17ColliF@fulford.york.sch.uk>
2022-09-12 15:16:41 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int r = rnd.Next(1, 10);
+            oddOrEven(r);
+        }
+
+        static int oddOrEven(int num)
+        {
+            int rem = num % 2;
+            if (rem == 0)
+            {
+                Console.WriteLine("Even");
+                return num;
+            }
+
+            else
+            {
+                Console.WriteLine("Odd");
+                return num;
+            }
+        }
+    }
+}
